5ef014f70494fbea74a15888c352e39216cf86ba
Billy <billynyh@gmail.com>
2013-01-08 17:32:14 +0800

    improve 9patch helper

--- README.md
+++ README.md
@@ -7,16 +7,16 @@ to9patch.py
 -----------
 create 9patch with simple options.
 
-usage: to9patch.py infile -x x_offset -y y_offset -o outfile
+usage: to9patch.py infile -t top_offset -l left_offset -b bottom_offset -r right_offset -o outfile
 
 offset can be ?% or ?px, default is 20%
 
 ### sample
 
     ./to9patch sample/sample_normal.png 
-    ./to9patch sample/sample_normal.png -x 20%
-    ./to9patch sample/sample_normal.png -x 20% -y 30%
-    ./to9patch sample/sample_normal.png -x 20% -y 30% -o sample/a.9.png
+    ./to9patch sample/sample_normal.png -t 20%
+    ./to9patch sample/sample_normal.png -t 20% -l 30%
+    ./to9patch sample/sample_normal.png -t 20% -l 30% -o sample/a.9.png
 
 button_xml.py
 -------------
--- to9patch.py
+++ to9patch.py
@@ -30,29 +30,47 @@ def get_offset(offset, l):
     return default
 
 
-def to9patch(fname, offset_x, offset_y, outfile):
-    COLOR = (0,0,0,255)
-
+def process_file(fname, offsets, outfile):
     ori = Image.open(fname)
+    result = to9patch_from_offsets(ori, offsets)
+    result.save(outfile, "PNG")
+
+def to9patch_from_offsets(ori, offsets):
     (w,h) = ori.size
 
-    ox = get_offset(offset_x, w)
-    oy = get_offset(offset_y, h)
+    o_t = get_offset(offsets[0], h)
+    o_b = get_offset(offsets[2], h)
+    o_l = get_offset(offsets[1], w)
+    o_r = get_offset(offsets[3], w)
 
+
+    lines = [
+        (0, 1+o_l, 0, h-o_l),
+        (w+1, 1+o_r, w+1, h-o_r),
+        (1+o_t, 0, w-o_t, 0),
+        (1+o_b, h+1, w-o_b, h+1)
+    ]
+
+    result = to9patch_from_lines(ori, lines)
+    return result
+    
+
+def to9patch_from_lines(ori, lines):
+    COLOR = (0,0,0,255)
+
+    (w,h) = ori.size
     size = (w+2, h+2)
+
     result = Image.new('RGBA', size)
     result.paste(ori, (1,1))
     draw = ImageDraw.Draw(result)
-    draw.line( (0, 1+oy, 0, h-oy), fill=COLOR, width=1 )
-    draw.line( (w+1, 1+oy, w+1, h-oy), fill=COLOR, width=1 )
-    draw.line( (1+ox, 0, w-ox, 0), fill=COLOR, width=1 )
-    draw.line( (1+ox, h+1, w-ox, h+1), fill=COLOR, width=1 )
+    for line in lines:
+        draw.line(line, fill=COLOR, width=1)
     del draw
-
-    result.save(outfile, "PNG")
+    return result
 
 def print_usage():
-    print 'usage: to9patch.py infile -x x_offset -y y_offset -o outfile'
+    print 'usage: to9patch.py infile -t top_offset -l left_offset -b bottom_offset -r right_offset -o outfile'
     print 'offset can be ?% or ?px, default is 20%'
     sys.exit(2)
     
@@ -63,13 +81,16 @@ def main(argv):
         print_usage()
         
     fname = argv[1]
-    offset_x = None
-    offset_y = None
     outfile = None
+
+    o_t = None
+    o_l = None
+    o_b = None
+    o_r = None
 
     if arg_len > 2:
         try:
-            opts, args = getopt.getopt(argv[2:],"x:y:o:")
+            opts, args = getopt.getopt(argv[2:],"o:t:l:b:r:")
         except getopt.GetoptError:
             print_usage()
 
@@ -77,21 +98,31 @@ def main(argv):
             print opts
 
         for opt, arg in opts:
-            if opt == "-x":
-                offset_x = arg
-            elif opt == "-y":
-                offset_y = arg
-            elif opt == "-o":
+            if opt == "-o":
                 outfile = arg
+            elif opt == "-t":
+                o_t = arg
+            elif opt == "-l":
+                o_l = arg
+            elif opt == "-b":
+                o_b = arg
+            elif opt == "-r":
+                o_r = arg
 
-    if offset_y is None:
-        offset_y = offset_x
-    if offset_x is None:
-        offset_x = offset_y
     if outfile is None:
         outfile = get_9patch_name(fname)
    
-    to9patch(fname, offset_x, offset_y, outfile)
+    if o_t is None:
+        o_t = o_b
+    if o_b is None:
+        o_b = o_t
+    if o_l is None:
+        o_l = o_r
+    if o_r is None:
+        o_r = o_l
+
+    offsets = (o_t, o_l, o_b, o_r)
+    process_file(fname, offsets, outfile)
 
 if __name__=="__main__":
     main(sys.argv)
